ed4cdf83ff7285c45ee7904c4e78aa14d6d194ea
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-14 23:45:14 +0200

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelFor
+{
+    public class Loops
+    {
+        [Params(100, 1_000, 10_000)]
+        public int limit;
+
+        [Benchmark]
+        public void NormalLoop()
+        {
+            for (int i = 0; i < limit; i++)
+            {
+                Console.WriteLine($"Zwykla petla {i}");
+            }
+        }
+
+        [Benchmark]
+        public void ParallelLoop()
+        {
+            Parallel.For(0, limit, i =>
+            {
+                Console.WriteLine($"Rownolegla petla {i}");
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System.Reflection;
+
+namespace ParallelFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = 1_000_000;
+            BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
+        }
+    }
+}
